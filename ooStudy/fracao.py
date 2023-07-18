40cac8d3cb077d3b6cb2b3d9d32b6a366d0d96cc
Romulo Santana <romulomsantana@hotmail.com>
2023-07-17 23:05:59 -0300

    create fraction class, attributes and methods

---
+++ ooStudy/fracao.py
@@ -0,0 +1,47 @@
+
+
+# atributos
+# 1. numerador; 2. denominador
+
+# metodos
+# somar; subtrair; multiplicar; dividir; inverter; inverter sinal; simplificar
+
+class Fraction:
+
+    def __init__(self, numerator, denominator):
+        self._numerator = numerator
+        if denominator == 0:
+            self._denominator = 1
+        else:
+            self._denominator = denominator
+
+    def sum(self, fraction):
+        if self._denominator == fraction._denominator:
+            num = self._numerator + fraction._numerator
+            den = self._denominator
+            return Fraction(num, den)
+        num = self._numerator * fraction._denominator - fraction._numerator * self._denominator
+        den = self._denominator * fraction._denominator
+        if num < 0:
+            num = -num
+        return Fraction(num, den)
+    
+    def subtract(self, fraction):
+        return self.sum(fraction.reverseSignal())
+
+    def multiply(self, fraction):
+        num = self._numerator * fraction._numerator
+        den = self._denominator * fraction._denominator
+        return Fraction(num, den)
+    
+    def divide(self, fraction):
+        return self.multiply(fraction.reverse())
+
+    def reverse(self):
+        return Fraction(self._denominator, self._numerator)
+
+    def reverseSignal(self):
+        return Fraction(-self._numerator, self._denominator)
+    
+    def simplify(self):
+        pass
---
+++ ooStudy/main.py
@@ -0,0 +1,12 @@
+from fracao import Fraction
+
+
+a = Fraction(4, 5)
+b = Fraction(3, 5)
+
+sumab = a.sum(b)
+
+mul = b.multiply(a)
+
+rev = a.reverse()
+print(rev._denominator)
