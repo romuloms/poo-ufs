77e708f073917afd2e88df9554fc7ae06707b205
Romulo Santana <romulomsantana@hotmail.com>
2023-07-19 22:40:48 -0300

    update notes class and main

--- notebook/main.py
+++ notebook/main.py
@@ -3,10 +3,30 @@ from notebook import Notebook
 
 myNotebook = Notebook()
 
-myNotebook.storeNote("comprar pao")
-myNotebook.storeNote("fazer tarefa")
-# n = myNotebook.numberOfNotes()
-# myNotebook.showNote(1)
-# myNotebook.removeNote("fazer tarefa")
+note1 = input("Digite a primeira nota: ")
+note2 = input("Digite a segunda nota: ")
+note3 = input("Digite a terceira nota: ")
 
+myNotebook.storeNote(note1)
+myNotebook.storeNote(note2)
+myNotebook.storeNote(note3)
+
+print("Numero de notas: ", myNotebook.numberOfNotes())
+print("Notas: ")
 myNotebook.listNotes()
+
+searchedNote = int(input("Digite o indice da nota que quer encontrar: "))
+myNotebook.showNote(searchedNote)
+
+removedNote = input("Digite a nota que quer remover: ")
+myNotebook.removeNote(removedNote)
+
+print("Numero de notas: ", myNotebook.numberOfNotes())
+
+print("metodo showNoteRandom:")
+myNotebook.showNoteRandom()
+
+print("metodo showMultiNoteRandom:")
+myNotebook.showMultiNoteRandom(3)
+
+myNotebook.printHasNotes()
--- notebook/notebook.py
+++ notebook/notebook.py
@@ -1,25 +1,56 @@
-class Notebook:
-    def __init__(self):
+import random
+
+
+class Notebook: 
+    def __init__(self): 
         self.__notes = list()
 
-    def storeNote(self, note):
+    def storeNote(self, note): 
         self.__notes.append(note)
 
-    def numberOfNotes(self):
+    def numberOfNotes(self): 
         return len(self.__notes)
     
-    def listNotes(self):
-        for note in self.__notes:
-            print(note)
-    
-    def showNote(self, noteNumber):
-        if (noteNumber < 0) or (noteNumber >= self.numberOfNotes()):
-            print("Valor invalido.")
-        else:
+    def listNotes(self): 
+        for note in self.__notes: print(note)
+
+    def showNote(self, noteNumber): 
+        if (noteNumber<0) or (noteNumber>=self.numberOfNotes()): 
+            print("Valor invalido") 
+        else: 
             print(self.__notes[noteNumber])
-    
-    def removeNote(self, note):
-        if note in self.__notes:
-            self.__notes.remove(note)
+
+    def removeNote(self, note): 
+        if note in self.__notes: 
+            self.__notes.remove(note) 
+        else: 
+            print("Nota invalida")
+
+    def hasNotes(self): 
+        if len(self.__notes) > 0: 
+            return True
+        else: 
+            return False
+
+    def compareNote(self, note):
+        if self.__notes.count(note) != 0:
+            return True
         else:
-            print("Nota invalida.")
+            return False
+
+    def showNoteRandom(self):
+        randomNumber = random.randint(0, len(self.__notes)-1)
+        print(self.__notes[randomNumber])
+
+    def showMultiNoteRandom(self, notes):
+        i = 1
+        while i <= notes:
+            randomNumber = random.randint(0, len(self.__notes)-1)
+            print(self.__notes[randomNumber])
+            i += 1
+
+    def printHasNotes(self):
+        if self.hasNotes():
+            print("Existem notas na lista")
+        else:
+            print("Lista vazia")
