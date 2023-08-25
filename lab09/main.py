3a346e30d848f1fde54b9e89fbf1cb9d4e108de4
Romulo Santana <romulomsantana@hotmail.com>
2023-08-25 14:05:01 -0300

    create test functions on main

--- lab09/main.py
+++ lab09/main.py
@@ -4,41 +4,32 @@ conta1 = Conta("Robson", 1234500)
 
 conta1.deposito(1000)
 conta1.imprimeConta()
-successful = False
-tentativas = 0
 
-while (not successful) and (tentativas < 3):
-    try:
-        conta1.saque(1500)
-        successful = True
-    except ValueError as erro:
-        print(erro)
-        tentativas += 1
-
-if not successful:
-    print("Nao foi possivel realizar o saque")
+def tentarSaque(valor):
+    sucesso = False
     tentativas = 0
+    while (not sucesso) and (tentativas < 3):
+        try:
+            conta1.saque(valor)
+            sucesso = True
+        except ValueError as erro:
+            print(erro)
+            tentativas += 1
 
-while (not successful) and (tentativas < 3):
-    try:
-        conta1.saque(580)
-        successful = True
-    except ValueError as erro:
-        print(erro)
-        tentativas += 1
+    if not sucesso:
+        print("Nao foi possivel realizar o saque")
+        tentativas = 0
 
-flagDeposito = False
-tentD = 0
-while (not flagDeposito) and (tentD < 3):
-    try:
-        conta1.deposito(-580)
-        successful = True
-    except ValueError as erro:
-        print(erro)
-        tentD += 1
-
-if not successful:
-    print("Nao foi possivel realizar o saque")
+def tentarDeposito(valor):
+    sucesso = False
+    tentativas = 0
+    while (not sucesso) and (tentativas < 3):
+        try:
+            conta1.deposito(-580)
+            sucesso = True
+        except ValueError as erro:
+            print(erro)
+            tentativas += 1
 
 try:
     conta1.setNomeCorrentista("")
