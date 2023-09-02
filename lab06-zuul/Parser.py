3afe5aee5b7f5975761fe8b1ff6c5ebf10ffc318
Romulo Santana <romulomsantana@hotmail.com>
2023-09-02 17:27:01 -0300

    fix class

--- lab06-zuul/Parser.py
+++ lab06-zuul/Parser.py
@@ -1,32 +1,28 @@
 from CommandWords import CommandWords
 from Command import Command
 
-class Parser:
-    def __init__(self):
-        self.__commands = CommandWords()
+class Parser: 
+    # create a parser to read commands from the terminal
+    def __init__(self): 
+        self.__commands = CommandWords() # holds all valid command words
 
+    # return user command
     def getCommand(self):
-        input_line = input("> ")
-        words = input_line.split()
-        print(words)
+        word1 = None
+        word2 = None
 
-        if len(words) > 0:
-            word1 = words[0]
-            if len(words) > 1:
-                word2 = words[1]
-            else:
-                word2 = None
+        inputLine = input("> ").split()
+
+        # find up to two words on the line
+        if(len(inputLine) >= 1):
+            word1 = inputLine[0]
+        if(len(inputLine) == 2):
+            word2 = inputLine[1]
+
+        # check whether this word is known. if so, create a command
+        # if not, create a null command (for unknown command).
+        if(self.__commands.isCommand(word1)):
+            return Command(word1, word2)
+
         else:
-            word1 = None
-            word2 = None
-
-        return Command(word1, word2)
-
-    def showCommands(self):
-        self.__commands.showAll()
-
-# Exemplo de uso
-parser = Parser()
-parser.showCommands()
-command = parser.getCommand()
-print(f"Command word: {command.getCommandWord()}, Second word: {command.getSecondWord()}")
+            return Command(None, word2)
