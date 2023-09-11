73c73d0516ed4f9c56fe17c6ba8605223b3f2505
Romulo Santana <romulomsantana@hotmail.com>
2023-09-11 17:55:25 -0300

    try zip feature

---
+++ pyLoops/enumerate.py
@@ -0,0 +1,4 @@
+planetas = ["Mercurio", "Venus", "Terra", "Marte", "Jupiter", "Saturno", "Urano", "Netuno"]
+
+for i, planeta in enumerate(planetas):
+    print(i, planeta)
---
+++ pyLoops/zip.py
@@ -0,0 +1,6 @@
+musicas = ["another brick in the wall", "stairway to heaven", "we will rock you", "my generation"]
+horarios = ["19:00", "19:45", "20:10", "20:30"]
+
+for musica, horario in zip(musicas, horarios):
+    print(musica, horario)
+    
